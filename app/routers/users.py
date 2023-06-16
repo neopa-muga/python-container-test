d08b52e63b457ff252d11d1a34dd54fba0a860df
Muga <muga@neopa.jp>
2023-06-16 08:49:25 +0000

    added a dependency to an endpoint

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,16 +1,33 @@
-from fastapi import APIRouter
+from typing import Annotated, Union
+from fastapi import APIRouter, Cookie, Depends
 
 router = APIRouter(
-    prefix="/items",
-    tags=["items"],
+    prefix="/users",
+    tags=["users"],
     # dependencies=[Depends(get_token_header)],
     responses={404: {"description": "Not found"}},
 )
 
 
-@router.get("/", tags=["users"])
-async def read_users():
-    return [{"username": "Rick"}, {"username": "Morty"}]
+def query_extractor(q: Union[str, None] = None):
+    return q
+
+
+def query_or_cookie_extractor(
+    q: Annotated[str, Depends(query_extractor)],
+    last_query: Annotated[Union[str, None], Cookie()] = None,
+):
+    if not q:
+        return last_query
+    return q
+
+
+@router.get("/", tags=["users"], summary="with dependency")
+async def read_users(
+    query_or_default: Annotated[str, Depends(query_or_cookie_extractor)]
+):
+    """get users"""
+    return {"q_or_cookie": query_or_default}
 
 
 @router.get("/me", tags=["users"])
